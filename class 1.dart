bd3c8736c63f55fd4323511ad64a2fdb98e6617d
TeamARahman <abdulrahmanmangi035@gmail.com>
2024-10-19 23:12:54 -0700

    create a class 1.dart file & class 2.dart & bool conditions.dart

---
+++ class 1.dart
@@ -0,0 +1,36 @@
+void main() {
+  print("hello world");
+  // jo bhi inerted comma mn likha hua hoga wo khlie ga (String)
+  
+  print(12); 
+  //agar koe number inerted comma mn nhiu likha hua hoga wo khlie ga (intiger)
+
+  print (hello world);
+  //agr koe word inverted comma mn nhi likha hoga wo khliega ga (variable)
+
+  var age = 12;
+  print(age);
+  // y hmara variable h is mn hm chezo ko store krte hn
+
+  // jo bhi code double slash (//) k bad likhe g wo comment khliega wo sirf apko show hoga output mn show nhi hoga
+
+  int code =1908;
+  // jo bhi hm number likhe g wo (int) mn likhe g
+  
+  String name ="TeamARahman";
+  // jo bhi name hm likhe g wo (String) mn likhe g
+
+
+  // kisi bhi variable ko hm dubara nhi bna skte. EXE= age,name etc..
+
+  double ratio =2.586;
+  // kisi bhi decimale wale number ko hm (double) mn likhe g 
+
+  num all =90;
+  // is data type mn hm koe bhi number likh skte hn chye wo intiger ho ya decimale.
+
+
+
+
+      //wish you best of luck duao mn yad rkhiega aj ka hmara class yhae p khtm hota h kudha hafiz.
+}
---
+++ class 2.dart
@@ -0,0 +1,20 @@
+void main() {
+  bool abc = 12 > 17;
+  print(abc);
+  // agr mn (bool) likho ga to iski madad s mn kisi bhi condition ko chck krskta ho k y true h ya false.
+
+//              ( ARTHIMATICS OPERATORS )
+
+// + , - , * , / , ~/ , % , ++ , --     <--- THESE OPERATORS ARE SIMPLE THAT USE IN BASIC MATH PLUS MINUS MULTPLY & DEVIDE
+
+// US K BAD ATA H (~/) Y WALA JO OPERATOR H HMARA Y RETURN MN INTIGER DETA  H Exe: AGR KOE DOTED NUMBER RETURN MN AE GA TB BHI Y HME INTIGER RETURN MN DEGA US KO ROUND OFF KRKE.
+// US K BAD (%) Y SIGN HMARA CODING MN GET THE REMINDER OF INTIGER USE HOTA H
+// <= LESS THAN EQUAL TO  |   == EQUAL TO  |  >= GRATER THAN EQUAL TO |  != NOT EQUAL TO  |
+
+
+
+
+ //wish you best of luck duao mn yad rkhiega aj ka hmara class yhae p khtm hota h kudha hafiz.
+}
+
+}
