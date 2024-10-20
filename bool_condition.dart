9d06948904f4015356c976b37a217cf84979ec07
TeamARahman <abdulrahmanmangi035@gmail.com>
2024-10-19 22:15:31 -0700

    add file bool_condition

---
+++ bool_condition.dart
@@ -0,0 +1,19 @@
+void main() {
+  int abc = 34;
+  int a = 21;
+  int q = 12;
+  int k = 8;
+  int i = a - q; //9
+
+  bool w = abc > a && !(a < abc);
+//          false
+
+  bool e = !w || q != k;
+  //         true
+
+  bool o = e && i > 4;
+//           true
+
+  print(!(!o || e));
+//       (false)
+}
