3d7f9387c03fe1a35ca6f1d39b180b974dccc4c5
TeamARahman <abdulrahmanmangi035@gmail.com>
2024-10-20 00:26:08 -0700

    create a folder of class 1 & create a folder of class 2 & create a folder of class 3

---
+++ class 3/class 3.dart
@@ -0,0 +1,19 @@
+void main() {
+  //   even or odd number checking program   //
+
+  int number = 2;
+  if (number % 2 == 0) {
+    print("even number");
+  } else {
+    print("odd number");
+  }
+
+// not completed project
+
+  // int leap_year = 2003;
+  // if (leap_year% == ) {
+  //   print("leap year");
+  // } else {
+  //   print("simple year");
+  // }
+}
